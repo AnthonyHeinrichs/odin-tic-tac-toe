4b9f299d08e3168698312b6ea8b460b1f9815342
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-10-04 19:09:56 +0200

    Grabbing all elements needed to start game or display content on the fly

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('loaded')
+const board = document.getElementById('playBoard')
+const boardCells = document.querySelectorAll('[board-cell]')
+const newGame = document.getElementById('newGame')
+const winner = document.getElementById('winner')
+const winnerName = document.getElementById('winnerName')
+const vsPlayer = document.getElementById('vsPlayer')
+const vsComputer = document.getElementById('vsComputer')
+
+
