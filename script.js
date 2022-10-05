3f5baabe7d68a86c881a71563569b084ab2741af
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-10-05 16:00:31 +0200

    Added following game logic: - on box hover, depending on turn, an x or o appears before selection - you can now select an empty box - you can not select a box that has already been selected - after selection, the game changes player turns

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
 const board = document.getElementById('playBoard')
-const boardCells = document.querySelectorAll('[board-cell]')
+const boardCells = document.querySelectorAll('[boardCell]')
 const newGame = document.getElementById('newGame')
 const winner = document.getElementById('winner')
 const winnerName = document.getElementById('winnerName')
 const vsPlayer = document.getElementById('vsPlayer')
 const vsComputer = document.getElementById('vsComputer')
-let playerOneTurn = true
+let xTurn = true
 
-const startGame = () => {
-  newGame.classList.add('hidden')
-  winner.classList.add('hidden')
-}
-
-const gameOver = () => {
-  board.classList.add('hidden')
-}
-
-vsPlayer.addEventListener('click', startGame)
 
 const winningCombinations = [
 	[0, 1, 2],
@@ -28,4 +18,65 @@ const winningCombinations = [
 	[0, 4, 8],
 	[2, 4, 6]
 ]
+
+const startGame = () => {
+  newGame.classList.add('hidden')
+  winner.classList.add('hidden')
+  boardCells.forEach(cell => {
+    cell.classList.remove('xChecked')
+    cell.classList.remove('oChecked')
+    cell.removeEventListener('click', gameRound)
+    cell.addEventListener('mouseover', addHover)
+    cell.addEventListener('click', gameRound, {once:true})
+  })
+}
+
+const gameRound = (e) => {
+  const cell = e.target
+  const playerClass = xTurn ? 'xChecked' : 'oChecked'
+  placePlayerClass(cell, playerClass)
+  changePlayer()
+}
+
+const placePlayerClass = (cell, player) => {
+  cell.classList.add(player)
+}
+
+const changePlayer = () => {
+  xTurn = !xTurn
+}
+
+const addHover = (e) => {
+  const cell = e.target
+  const cellClasses = cell.classList 
+  if (cellClasses.contains('xChecked') || cellClasses.contains('oChecked')) {
+    return
+  } else if (xTurn) {
+    cellClasses.add('x')
+    cellClasses.remove('o')
+  } else {
+    cellClasses.add('o')
+    cellClasses.remove('x')
+  }
+}
+
+const handleCellHover = () => {
+  if (xTurn) {
+    boardCells.forEach(cell => {
+      cell.classList.remove('o')
+      cell.classList.add('x')
+    })
+  } else {
+    boardCells.forEach(cell => {
+      cell.classList.remove('x')
+      cell.classList.add('o')
+    })
+  }
+}
+
+vsPlayer.addEventListener('click', startGame)
+
+const gameOver = () => {
+  board.classList.add('hidden')
+}
 
