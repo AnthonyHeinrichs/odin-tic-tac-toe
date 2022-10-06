52dbbf80ea8314a6336b904ace374222738453d7
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-10-06 15:14:47 +0200

    Added comments to script, removed unecessary function, updated design

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-const board = document.getElementById('playBoard')
 const boardCells = document.querySelectorAll('[boardCell]')
 const newGame = document.getElementById('newGame')
-const winner = document.getElementById('winner')
 const vsPlayer = document.getElementById('vsPlayer')
-const vsComputer = document.getElementById('vsComputer')
 let xTurn = true
 
-
+// Array of arrays that define the different combinations of boxes for a win
 const winningCombinations = [
 	[0, 1, 2],
 	[3, 4, 5],
@@ -18,7 +15,11 @@ const winningCombinations = [
 	[2, 4, 6]
 ]
 
+// Function that runs when a user selects new game
+// Hides some css elements and adds event listeners board cells
+// to listen for clicks and hover over
 const startGame = () => {
+  const winner = document.getElementById('winner')
   newGame.classList.add('hidden')
   winner.classList.add('hidden')
   boardCells.forEach(cell => {
@@ -30,12 +31,16 @@ const startGame = () => {
   })
 }
 
+//Check if every board cell contains a class for x or o to determine if
+// all cells have been selected
 const checkForDraw = () => {
   return [...boardCells].every(cell => {
     return cell.classList.contains('xChecked') || cell.classList.contains('oChecked')
   })
 }
 
+// Check for who's turn it is, then add new x or o mark to the board cell
+// then check if a player won, or if there is a draw, else change player's turn
 const gameRound = (e) => {
   const cell = e.target
   const playerClass = xTurn ? 'xChecked' : 'oChecked'
@@ -49,15 +54,18 @@ const gameRound = (e) => {
   }
 }
 
+// Add new class to the board cell if selected by a player
 const placePlayerClass = (cell, player) => {
   cell.classList.add(player)
-  console.log(checkForDraw())
 }
 
+// Change the player's turn
 const changePlayer = () => {
   xTurn = !xTurn
 }
 
+// When hoving over the board cells, add the hover classes 
+// so a gray x or o will show depending on player turn
 const addHover = (e) => {
   const cell = e.target
   const cellClasses = cell.classList 
@@ -72,22 +80,8 @@ const addHover = (e) => {
   }
 }
 
-const handleCellHover = () => {
-  if (xTurn) {
-    boardCells.forEach(cell => {
-      cell.classList.remove('o')
-      cell.classList.add('x')
-    })
-  } else {
-    boardCells.forEach(cell => {
-      cell.classList.remove('x')
-      cell.classList.add('o')
-    })
-  }
-}
-
-vsPlayer.addEventListener('click', startGame)
-
+// Checks the current board cells against the winning combination array
+// to determine if any of cells match the combination array passed
 function checkWin(turn) {
 	return winningCombinations.some(combination => {
 		return combination.every(index => {
@@ -96,14 +90,18 @@ function checkWin(turn) {
 	})
 }
 
+// Checks if there was a draw or if a player won and 
+// updates the html elements to show if it was a draw or who won
 const gameOver = (draw) => {
   const winnerName = document.getElementById('winnerName')
   if (draw) {
     winnerName.innerText = 'Draw!'
   } else {
-    winnerName.innerText = `Player ${xTurn ? 'one' : 'two'} won!`
+    winnerName.innerText = `Player ${xTurn ? '1' : '2'} won!`
   }
   newGame.classList.remove('hidden')
   winner.classList.remove('hidden')
 }
 
+// Runs function to start game when new game is clicked
+vsPlayer.addEventListener('click', startGame)
